33c4199dc6725378f96214793ec0aa73e7a99923
杨仕明 <shiming.y@qq.com>
2024-05-10 13:19:19 +0800

    fix resolvers.ts

--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -2,15 +2,15 @@
  * @Author: 杨仕明 shiming.y@qq.com
  * @Date: 2024-04-28 16:39:19
  * @LastEditors: 杨仕明 shiming.y@qq.com
- * @LastEditTime: 2024-05-08 21:47:27
- * @FilePath: /lulab_graphql_mock/src/schema/resolvers.ts
+ * @LastEditTime: 2024-05-09 21:33:27
+ * @FilePath: /lulab_graphql_mock/src/resolvers/resolvers.ts
  * @Description: 
  * 
  * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
  */
 
 
-// 该文件用于业务层获取的数据，不非编辑
+// 该文件用于业务层获取的数据，不需要编辑
 module.exports = {
     Query: {
         hello: () => 'Hello, World!',
